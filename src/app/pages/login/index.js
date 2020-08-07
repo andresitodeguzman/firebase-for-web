aced7dda0f98e959894e07e133c759b5afd169cb
Andresito de Guzman <deguzman.andresito@gmail.com>
2020-08-07 14:05:15 +0800

    feat: remove creds

--- src/app/pages/login/index.js
+++ src/app/pages/login/index.js
@@ -23,14 +23,21 @@ export class PageComponent extends navigator(LitElement) {
 
     googleLogin() {
         const navigate = this.navigate;
+
         firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+
         .then(function() {
             const provider = new firebase.auth.GoogleAuthProvider();
+
             return firebase.auth().signInWithPopup(provider).then(res => {
+
                 navigate('/');
+
             }).catch(e => {
+
                 console.error(e);
                 alert('An error occurred');
+
             });
         })
         .catch(function(error) {
--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 window.addEventListener('load', () => {
-    window.firebase = f.initializeApp({
-        apiKey: "AIzaSyDXa742e7GB4hx7uLTdO0pTbPu4fB3Th2Y",
-        authDomain: "sarap-app.firebaseapp.com",
-        databaseURL: "https://sarap-app.firebaseio.com",
-        projectId: "sarap-app",
-        storageBucket: "sarap-app.appspot.com",
-        messagingSenderId: "91211460712",
-        appId: "1:91211460712:web:6d51ae14b1ec01a1c30bdd",
-        measurementId: "G-PG3Y17ZEMS"
-    });
+    window.firebase = f.initializeApp();
 
     firebase.auth().onAuthStateChanged(function(user) {
         if (!user && window.location.pathname !== '/login') {
